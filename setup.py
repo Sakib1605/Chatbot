5cc6fda0a462452e970a2845448384feb4e91a8e
Mohammad Sakibul Islam <sakibul1605@gmail.com>
2025-11-26 11:42:33 +0600

    Initial clean commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="chatbot",
+    version="0.1.0",
+    author="Mohammad Sakibul Islam",
+    author_email="sakibul1605@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,16 @@
+## Creating directory
+mkdir -p src
+mkdir -p research
+
+##Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+
+echo "Directory and Files Created Successfully"
